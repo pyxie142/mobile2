4c85b623314d7f62991dfc23e2625ddd70feaa8c
pyxie142 <pietra.almeida.nunes@escola.pr.gov.br>
2025-11-18 09:50:44 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-const { useState } = require("react")
-
 function App() {
-  const(message, setMessage) = useState('ola mundo')
+  const[message, setMessage] = useState('ola mundo');
+  return (
+<div>
+  <h1>{message}</h1>
+  <button onClick={
+    {}=>{setMessage('ola fui criado');}} 
+    ></button>
+</div>
+  )
 }
+
+export default App
