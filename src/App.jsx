3be0c905009360a0e4c53c69e7ddb8900d8e1849
pyxie142 <pietra.almeida.nunes@escola.pr.gov.br>
2025-11-19 09:47:48 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-function App() {
-  const[message, setMessage] = useState('ola mundo');
+import { useState } from "react";
+
+function App(){/* cria a função componente*/
+  ////let message = 'olá mundo';/* variavel em java script*/
+const [message, setMessage] = useState('olá, mundo'); //o set message altera o valor do state, useState tem que ter a mensagem inicial
+
+  ////state (estado) variavel que atualiza o componente
   return (
-<div>
-  <h1>{message}</h1>
-  <button onClick={
-    {}=>{setMessage('ola fui criado');}} 
-    ></button>
-</div>
-  )
+  <div>
+    <h1>{message}</h1>{/*jsx em ação */}
+    <button onClick={() =>{
+      setMessage ('Olá, fui clicado');
+    }}>Mudar Mensagem</button>{/*cria um botão, o java script sempre vai dentro da chaves */ }
+  </div>/* o que ela vai retornar*/
+  );
 }
 
-export default App
+export default App /* exporta o app*/
